7246557735cf537b0e79fbad9fd5c1ed4703bdde
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-04 20:21:48 +0100

    Added foo bar function.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  foo = () => 'Bars';
+  render() {
+    const name = 'John Doe';
+    return (
+      <div className="App">
+        <h1>Hello {name.toLocaleUpperCase()}</h1>
+        <p>This is the function foo -> {this.foo()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
