fe8685c3abd6034138e4084c9c5f6e108fd9e0db
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-05 14:48:14 +0100

    Functrional component with props

--- src/components/layout/users/UserItem.js
+++ src/components/layout/users/UserItem.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Useritems extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt='Avatar'
-          className='round-img'
-          style={{ width: '3.75em' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            Profile
-          </a>
-        </div>
+const Useritems = props => {
+  const { avatar_url, login, html_url } = props.user;
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt='Avatar'
+        className='round-img'
+        style={{ width: '3.75em' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          Profile
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Useritems;
