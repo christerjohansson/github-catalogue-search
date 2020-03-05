c3c3a7622fb7d54daf1470f17f0ac94361416ea3
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-05 16:04:40 +0100

    Finished up basic API call to Github

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  async componentDidMount() {
-    const res = await axios.get('https://api.github.com/users');
+  state = {
+    users: [],
+    loading: false
+  };
 
-    console.log(res.data);
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
   }
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
