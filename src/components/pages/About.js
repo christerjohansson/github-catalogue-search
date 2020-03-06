940b92d658d6625b7e250717d55c95b0575a7df0
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-06 16:49:52 +0100

    Router with Link added

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this application</h1>
+      <p>This small application searches the Github api for users</p>
+      <p>Version: 0.0.2 (Alpha)</p>
+    </Fragment>
+  );
+};
+
+export default About;
