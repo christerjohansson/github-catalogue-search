4c22bfd284d2a1cd12babd729d8d68a2fad0e21e
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-06 13:26:13 +0100

    refactored app.js

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
@@ -47,9 +48,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
