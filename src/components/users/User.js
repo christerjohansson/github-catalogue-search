a0bcd95e14be991c64a8b6743b5258b9a13f4b0a
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-17 14:47:33 +0100

    Added colors to hireable status and some badges

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -22,13 +22,16 @@ export class User extends Component {
       location,
       bio,
       blog,
+      company,
+      following,
+      followers,
       login,
       html_url,
-      followers,
       public_repos,
       public_gists,
       hireable,
-      created_at
+      created_at,
+      updated_at
     } = this.props.user;
 
     const { loading } = this.props;
@@ -38,18 +41,51 @@ export class User extends Component {
 
     return (
       <Fragment>
-        <Link to='/' className='btn btn-light'>Go back</Link>
+        <Link to='/' className='btn btn-dark'>Go back</Link>
         ID: {''} {id} | 
-        Hireable: {''}
-        {hireable ? <i className="fas fa-check text-success" /> : <i className="fas fa-times-circle text-danger" />} | 
-        Member since: {(new Date(created_at)).toLocaleDateString('en-US', DATE_OPTIONS)}
+        {hireable ? <div className="badge badge-success">Hireable</div> : <div className="badge badge-danger">Not hireable</div>} | 
+        Member since: {(new Date(created_at)).toLocaleDateString('en-US', DATE_OPTIONS)} | 
+        Last seen: {(new Date(updated_at)).toLocaleDateString('en-US', DATE_OPTIONS)}
 
         <div className="card grid-2">
           <div className="all-center">
-            <img src={avatar_url} alt={name} className="round-img" style={{width: '5em'}}/>
+            <img src={avatar_url} alt={name} className="round-img" style={{width: '7.5em'}}/>
+            <h1>{name}</h1>
+            <p>location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1" target="_blank" rel="noopener noreferrer">Visit my profile</a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username: </strong> {login}
+                  </Fragment>}
+              </li>
+              <li>
+                {login && <Fragment>
+                  <strong>Company: </strong> {company}
+                  </Fragment>}
+              </li>
+              <li>
+                {login && <Fragment>
+                  <strong>Website: </strong> <a href={blog} target="_blank" rel="noopener noreferrer">{blog}</a>
+                  </Fragment>}
+              </li>
+            </ul>
           </div>
         </div>
-
+        <div className="card text-center">
+                <div className="badge badge-primary">Followers: {followers}</div>
+                <div className="badge badge-success">Following: {following}</div>
+                <div className="badge badge-danger">Public repos: {public_repos}</div>
+                <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
