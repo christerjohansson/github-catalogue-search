f8557c87a8f6d215bf4dc6e48a45d34b28e4b2a8
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-17 18:42:11 +0100

    Minor corrections

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class App extends Component {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=$
+      `https://api.github.com/users/${login}/repos?per_page=10&sort=created:asc&client_id=$
       {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
