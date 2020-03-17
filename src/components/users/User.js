ab593f192c3a37f5c3379f699d24b85895bbb525
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-17 13:56:28 +0100

    Added date formatting to user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -16,6 +16,7 @@ export class User extends Component {
 
   render() {
     const {
+      id,
       name,
       avatar_url,
       location,
@@ -26,16 +27,28 @@ export class User extends Component {
       followers,
       public_repos,
       public_gists,
-      hireable
+      hireable,
+      created_at
     } = this.props.user;
 
     const { loading } = this.props;
+    const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
 
     if (loading) return <Spinner />
 
     return (
       <Fragment>
         <Link to='/' className='btn btn-light'>Go back</Link>
+        ID: {''} {id} | 
+        Hireable: {''}
+        {hireable ? <i className="fas fa-check text-success" /> : <i className="fas fa-times-circle text-danger" />} | 
+        Member since: {(new Date(created_at)).toLocaleDateString('en-US', DATE_OPTIONS)}
+
+        <div className="card grid-2">
+          <div className="all-center">
+            <img src={avatar_url} alt={name} className="round-img" style={{width: '5em'}}/>
+          </div>
+        </div>
 
       </Fragment>
     );
