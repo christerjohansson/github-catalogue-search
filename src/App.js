aa6d6641c6730b55deba0642bba43bc7110d94a7
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-18 23:09:34 +0100

    Created reducers & actions

--- src/App.js
+++ src/App.js
@@ -30,18 +30,7 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  // Searching users in the catalogue by
-  // asking API through props and component
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=$
-      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
+ 
 
   // Locate a single user on Github
   const getUser = async login => {
@@ -97,7 +86,6 @@ const App = () => {
               render={props => (
                 <Fragment>
                   <Search
-                    searchUsers={searchUsers}
                     clearUsers={clearUsers}
                     showClear={users.length > 0 ? true : false}
                     setAlert={displayAlert}
--- src/context/github/GithubState.jsx
+++ src/context/github/GithubState.jsx
@@ -19,23 +19,41 @@ const GithubState = props => {
     }
     const [state, dispatch] = useReducer(GitubReducer, initialState);
 
-    // Search users
+// Searching users in the catalogue by
+// asking API through props and component
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+        type: SEARCH_USERS,
+        payload: res.data.items
+    });
+  };
 
 
-    // Get User
+// Get User
 
-    // Get Repos
+// Get Repos
 
-    // Clears Users
+// Clears Users
 
-    // Set Loading
+
+// Set Loading
+const setLoading= () => dispatch({type: SET_LOADING});
 
     return <GithubContext.Provider
     value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
     }}
     >
     
