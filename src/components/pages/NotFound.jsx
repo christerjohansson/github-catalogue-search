df8b5b118e54c22fef9c785d4db50c12b38523e2
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-20 16:37:23 +0100

    Implemented Notfound and default home page.

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+    return (
+    <Fragment>
+        <h1>Not found!</h1>
+        <p className="lead">The page you are looking for, does not exist.</p>
+        <p>Go back to start page and try again.</p>
+    </Fragment>    
+    )
+}
+export default NotFound;
