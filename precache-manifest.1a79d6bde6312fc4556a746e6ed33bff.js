be162c8bd81fd18b382ef30c058bfcdbdc6d1292
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-21 13:31:48 +0100

    Updates

---
+++ precache-manifest.1a79d6bde6312fc4556a746e6ed33bff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1e29b583dd7c63820ed74d7e9801dc",
+    "url": "/github-catalogue-search/index.html"
+  },
+  {
+    "revision": "41d2e7bf45446397a72a",
+    "url": "/github-catalogue-search/static/css/main.dbd16520.chunk.css"
+  },
+  {
+    "revision": "e1365ce637e7a50a582a",
+    "url": "/github-catalogue-search/static/js/2.782e7b77.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/github-catalogue-search/static/js/2.782e7b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d2e7bf45446397a72a",
+    "url": "/github-catalogue-search/static/js/main.22d5e695.chunk.js"
+  },
+  {
+    "revision": "a877187cfbf10c93e5af",
+    "url": "/github-catalogue-search/static/js/runtime-main.71c6e806.js"
+  },
+  {
+    "revision": "5051e9517a98b48146b75cafc662e6a0",
+    "url": "/github-catalogue-search/static/media/831.5051e951.gif"
+  }
+]);
