262f314b0bc3d12de0b342cc5748ad60147fa004
Christer Johansson <christer.johansson@stenungsund.nu>
2020-03-21 13:51:33 +0100

    Modified router and scripts

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <Fragment>
       <h1>About this application</h1>
       <p>This small application searches the Github api for users</p>
-      <p>Version: 1.0 (202003)</p>
+      <p>Version: 1.1.0 (2020/03/21)</p>
     </Fragment>
   );
 };
